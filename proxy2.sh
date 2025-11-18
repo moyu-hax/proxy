328fae9f029345180a6988052c85a0cc66132c5b
moyu-hax <simplething168@outlook.com>
2025-11-18 15:23:53 +0800

    Update proxy2.sh

--- proxy2.sh
+++ proxy2.sh
@@ -271,7 +271,7 @@ while true; do
       clear
       echo -e "${purple}▶ 节点搭建脚本合集${re}"
       echo -e "${green}---------------------------------------------------------${re}"
-      echo -e "${white} 1. Hysteria2一键脚本        2. Reality一键脚本${re}"
+      echo -e "${cyan} 1. Hysteria2一键脚本        2. Reality一键脚本${re}"
       echo -e "${cyan} 3. Tuic-V5一键脚本${re}" # 新增Tuic选项
       echo -e "${yellow}---------------------------------------------------------${re}"
       echo -e "${skyblue} 0. 退出脚本${re}" # 优化：主菜单的0通常是退出
